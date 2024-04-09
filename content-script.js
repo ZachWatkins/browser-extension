183f14e418f3961ab493dde5eed422836d33b1f3
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2024-04-08 20:54:29 -0500

    Add docs

--- content-script.js
+++ content-script.js
@@ -9,13 +9,27 @@ chrome.runtime.onMessage.addListener(function (message) {
     }
 });
 
+/**
+ * Activate the extension's event handlers.
+ * @returns {void}
+ */
 function activate() {
     document.body.addEventListener('mousemove', logMouseMove);
 }
+
+/**
+ * Deactivate the extension's event handlers.
+ * @returns {void}
+ */
 function deactivate() {
     document.body.removeEventListener('mousemove', logMouseMove);
 }
 
+/**
+ * Log the mousemove event.
+ * @param {MouseEvent} e The mousemove event.
+ * @returns {void}
+ */
 function logMouseMove(e) {
     chrome.runtime.sendMessage({
         type: 'log',
